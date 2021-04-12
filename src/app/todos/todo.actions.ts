03e8c9b3073b625ea3f08b3a66c64c6611cb76a2
JohnCua <cuatumax@gmail.com>
2021-04-12 11:38:43 -0600

    Editar datos

--- src/app/todos/todo.actions.ts
+++ src/app/todos/todo.actions.ts
@@ -11,3 +11,8 @@ export const toggle = createAction(
     '[TODO] Toggle Todo',
     props<{ id: number }>()  //argumento
     );
+
+export const editar = createAction(
+    '[TODO] Editar Todo',
+    props<{ id: number, texto: string }>()  //argumento
+    );
